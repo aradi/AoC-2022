c1155639d59e906e02711900dc98794decdcef75
Bálint Aradi <aradi@uni-bremen.de>
2022-12-05 22:44:59 +0100

    Add second version of the Fortran solutions

--- 05/d05-2.f90
+++ 05/d05-2.f90
@@ -1,132 +1,63 @@
+! Second version of the solution for day 5
+! The input was inspired a lot by S. Lionel's solution at
+! https://github.com/sblionel/AOC2022-in-Fortran/blob/main/AOC05_1.f90
+!
 program d05_2
   implicit none
 
   integer, parameter ::maxlinelen = 1024
 
-  type :: string_t
-    character(:), allocatable :: content
-  end type string_t
+  character(maxlinelen) :: buffer
+  integer :: fp, iostat
+  integer :: nstacks, istack, nlayers, ilayer
+  character :: firstchar
+  character, allocatable :: stacks(:,:), newstacks(:,:)
+  character, allocatable :: dynbuffer(:), stacktops(:)
+  integer, allocatable :: stacksizes(:)
+  character(4) :: movestr, fromstr, tostr
+  integer :: amount, source, dest
 
-  type(string_t), allocatable :: lines(:)
-  type(string_t), allocatable :: stacks(:)
-  character(:), allocatable :: topcrates
-  integer, allocatable :: operations(:,:)
+  ! Determine nr. of stacks and layers
+  nstacks = 0
+  nlayers = 0
+  open(newunit=fp, file="input.dat", action="read")
+  do
+    read(fp, "(a)", iostat=iostat) buffer
+    if (iostat /= 0) exit
+    read(buffer, *) firstchar
+    if (firstchar /= "[") exit
+    nlayers = nlayers + 1
+    nstacks = max(nstacks, (len_trim(buffer) + 1) / 4)
+  end do
+  rewind(fp)
 
-  call read_lines("input.dat", lines)
-  call extract_input(lines, stacks, operations)
-  call apply_operations(operations, stacks)
-  call get_top_crates(stacks, topcrates)
-  print *, "Top crates: ", topcrates
+  ! Read in stacks
+  allocate(stacks(nlayers + 1, nstacks), source=" ")
+  do ilayer = 1, nlayers
+    read(fp, "(a)") buffer
+    dynbuffer = transfer(trim(buffer), ["c"])
+    stacks(ilayer + 1, 1 : (size(dynbuffer) + 1) / 4) = dynbuffer(2::4)
+  end do
+  stacks(:,:) = stacks(size(stacks, dim=1) : 1 : -1, :)
 
-contains
+  ! Find sizes and make new array which warrantedly can accomodate any potential stack sizes
+  stacksizes = findloc(stacks, " ", dim=1) - 1
+  allocate(newstacks(sum(stacksizes), nstacks))
+  newstacks(1 : nlayers, :) = stacks(1 : nlayers, :)
 
+  ! Read in and carry out move instructions
+  read(fp, *)
+  do
+    read(fp, *, iostat=iostat) movestr, amount, fromstr, source, tostr, dest
+    if (iostat /= 0) exit
+    newstacks(stacksizes(dest) + 1 : stacksizes(dest) + amount, dest) =&
+        & newstacks(stacksizes(source) - amount + 1 : stacksizes(source), source)
+    stacksizes(source) = stacksizes(source) - amount
+    stacksizes(dest) = stacksizes(dest) + amount
+  end do
+  close(fp)
 
-  subroutine read_lines(filename, lines)
-    character(*), intent(in) :: filename
-    type(string_t), allocatable :: lines(:)
-
-    integer :: fp, nlines, iline, iostat
-    character(maxlinelen) :: buffer
-
-    open(newunit=fp, file=filename, form="formatted", action="read", access="stream")
-    nlines = 0
-    do
-      read(fp, *, iostat=iostat)
-      if (iostat /= 0) exit
-      nlines = nlines + 1
-    end do
-
-    allocate(lines(nlines))
-    rewind(fp)
-    do iline = 1, nlines
-      read(fp, "(a)") buffer
-      lines(iline)%content = trim(buffer)
-    end do
-    close(fp)
-
-  end subroutine read_lines
-
-
-  subroutine extract_input(lines, stacks, operations)
-    type(string_t), intent(in) :: lines(:)
-    type(string_t), allocatable, intent(out) :: stacks(:)
-    integer, allocatable, intent(out) :: operations(:,:)
-
-    character(:), allocatable :: buffer
-    integer :: nlayers, nstacks, ilayer, istack, iline, pos
-    integer :: noperations, ioperation
-    character :: poschar
-    character(4) :: movestr, fromstr, tostr
-
-    nlayers = 0
-    nstacks = 0
-    do
-      buffer = adjustl(lines(nlayers + 1)%content)
-      if(buffer(1:1) /= "[") exit
-      nlayers = nlayers + 1
-      nstacks = max(nstacks, (len_trim(lines(nlayers)%content) + 1) / 4)
-    end do
-
-    allocate(stacks(nstacks))
-    do istack = 1, nstacks
-      stacks(istack)%content = ""
-    end do
-
-    do ilayer = 1, nlayers
-      iline = nlayers - ilayer + 1
-      do istack = 1, len(lines(iline)%content) / 4 + 1
-        pos = (istack - 1) * 4 + 2
-        poschar = lines(iline)%content(pos:pos)
-        if (poschar /= " ") stacks(istack)%content = stacks(istack)%content // poschar
-      end do
-    end do
-
-    noperations = size(lines) - nlayers - 2
-    allocate(operations(3, noperations))
-    do ioperation = 1, noperations
-      iline = nlayers + 2 + ioperation
-      associate(line => lines(iline)%content, operation => operations(:, ioperation))
-        read(line, *) movestr, operation(1), fromstr, operation(2), tostr, operation(3)
-      end associate
-    end do
-
-  end subroutine extract_input
-
-
-  subroutine apply_operations(operations, stacks)
-    integer, intent(in) :: operations(:,:)
-    type(string_t), intent(inout) :: stacks(:)
-
-    integer :: ioperation, fromstacklen
-    character(:), allocatable :: moved
-
-    do ioperation = 1, size(operations, dim=2)
-      associate(&
-          & amount => operations(1, ioperation),&
-          & fromstack => stacks(operations(2, ioperation)),&
-          & tostack => stacks(operations(3, ioperation)))
-        fromstacklen = len(fromstack%content)
-        moved = fromstack%content(fromstacklen - amount + 1 : fromstacklen)
-        tostack%content = tostack%content // moved
-        fromstack%content = fromstack%content(1 : fromstacklen - amount)
-      end associate
-    end do
-
-  end subroutine apply_operations
-
-
-  subroutine get_top_crates(stacks, topcrates)
-    type(string_t), intent(in) :: stacks(:)
-    character(:), allocatable, intent(out) :: topcrates
-
-    integer :: istack, stacksize
-
-    allocate(character(size(stacks)) :: topcrates)
-    do istack = 1, size(stacks)
-      stacksize = len(stacks(istack)%content)
-      topcrates(istack:istack) = stacks(istack)%content(stacksize : stacksize)
-    end do
-
-  end subroutine get_top_crates
+  stacktops = [(newstacks(stacksizes(istack), istack), istack = 1, nstacks)]
+  print *, "Top elements of the stacks: ", stacktops
 
 end program d05_2
