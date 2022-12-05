e9e5bda31f6bf9ffebd83e62ee0dc9aeedad2990
Bálint Aradi <aradi@uni-bremen.de>
2022-12-05 11:12:21 +0100

    Add solutions for day 5

---
+++ 05/d05-2.f90
@@ -0,0 +1,132 @@
+program d05_2
+  implicit none
+
+  integer, parameter ::maxlinelen = 1024
+
+  type :: string_t
+    character(:), allocatable :: content
+  end type string_t
+
+  type(string_t), allocatable :: lines(:)
+  type(string_t), allocatable :: stacks(:)
+  character(:), allocatable :: topcrates
+  integer, allocatable :: operations(:,:)
+
+  call read_lines("input.dat", lines)
+  call extract_input(lines, stacks, operations)
+  call apply_operations(operations, stacks)
+  call get_top_crates(stacks, topcrates)
+  print *, "Top crates: ", topcrates
+
+contains
+
+
+  subroutine read_lines(filename, lines)
+    character(*), intent(in) :: filename
+    type(string_t), allocatable :: lines(:)
+
+    integer :: fp, nlines, iline, iostat
+    character(maxlinelen) :: buffer
+
+    open(newunit=fp, file=filename, form="formatted", action="read", access="stream")
+    nlines = 0
+    do
+      read(fp, *, iostat=iostat)
+      if (iostat /= 0) exit
+      nlines = nlines + 1
+    end do
+
+    allocate(lines(nlines))
+    rewind(fp)
+    do iline = 1, nlines
+      read(fp, "(a)") buffer
+      lines(iline)%content = trim(buffer)
+    end do
+    close(fp)
+
+  end subroutine read_lines
+
+
+  subroutine extract_input(lines, stacks, operations)
+    type(string_t), intent(in) :: lines(:)
+    type(string_t), allocatable, intent(out) :: stacks(:)
+    integer, allocatable, intent(out) :: operations(:,:)
+
+    character(:), allocatable :: buffer
+    integer :: nlayers, nstacks, ilayer, istack, iline, pos
+    integer :: noperations, ioperation
+    character :: poschar
+    character(4) :: movestr, fromstr, tostr
+
+    nlayers = 0
+    nstacks = 0
+    do
+      buffer = adjustl(lines(nlayers + 1)%content)
+      if(buffer(1:1) /= "[") exit
+      nlayers = nlayers + 1
+      nstacks = max(nstacks, (len_trim(lines(nlayers)%content) + 1) / 4)
+    end do
+
+    allocate(stacks(nstacks))
+    do istack = 1, nstacks
+      stacks(istack)%content = ""
+    end do
+
+    do ilayer = 1, nlayers
+      iline = nlayers - ilayer + 1
+      do istack = 1, len(lines(iline)%content) / 4 + 1
+        pos = (istack - 1) * 4 + 2
+        poschar = lines(iline)%content(pos:pos)
+        if (poschar /= " ") stacks(istack)%content = stacks(istack)%content // poschar
+      end do
+    end do
+
+    noperations = size(lines) - nlayers - 2
+    allocate(operations(3, noperations))
+    do ioperation = 1, noperations
+      iline = nlayers + 2 + ioperation
+      associate(line => lines(iline)%content, operation => operations(:, ioperation))
+        read(line, *) movestr, operation(1), fromstr, operation(2), tostr, operation(3)
+      end associate
+    end do
+
+  end subroutine extract_input
+
+
+  subroutine apply_operations(operations, stacks)
+    integer, intent(in) :: operations(:,:)
+    type(string_t), intent(inout) :: stacks(:)
+
+    integer :: ioperation, fromstacklen
+    character(:), allocatable :: moved
+
+    do ioperation = 1, size(operations, dim=2)
+      associate(&
+          & amount => operations(1, ioperation),&
+          & fromstack => stacks(operations(2, ioperation)),&
+          & tostack => stacks(operations(3, ioperation)))
+        fromstacklen = len(fromstack%content)
+        moved = fromstack%content(fromstacklen - amount + 1 : fromstacklen)
+        tostack%content = tostack%content // moved
+        fromstack%content = fromstack%content(1 : fromstacklen - amount)
+      end associate
+    end do
+
+  end subroutine apply_operations
+
+
+  subroutine get_top_crates(stacks, topcrates)
+    type(string_t), intent(in) :: stacks(:)
+    character(:), allocatable, intent(out) :: topcrates
+
+    integer :: istack, stacksize
+
+    allocate(character(size(stacks)) :: topcrates)
+    do istack = 1, size(stacks)
+      stacksize = len(stacks(istack)%content)
+      topcrates(istack:istack) = stacks(istack)%content(stacksize : stacksize)
+    end do
+
+  end subroutine get_top_crates
+
+end program d05_2
---
+++ 05/d05-2.py
@@ -0,0 +1,33 @@
+with open("input.dat", "r") as fp:
+    lines = fp.readlines()
+
+stacklines = []
+nstacks = 0
+for line in lines:
+    if line.lstrip()[0] == '[':
+        stacklines.append(line)
+        # Note: line contains trailing '\n' character
+        nstacks = max(nstacks, (len(line) // 4))
+    else:
+        break
+stacklines.reverse()
+
+stacks = [[] for _ in range(nstacks)]
+for line in stacklines:
+    for istack, char in enumerate(line[1::4]):
+        if char != " ":
+            stacks[istack].append(char)
+
+operations = []
+for line in lines[len(stacklines) + 2 :]:
+    if not line.strip():
+        break
+    words = line.split()
+    operations.append((int(words[1]), int(words[3]), int(words[5])))
+
+for amount, source, target in operations:
+    stacks[target - 1].extend(stacks[source - 1][-amount:])
+    del stacks[source - 1][-amount:]
+
+topcrates = [stack[-1] for stack in stacks]
+print("Top crates: ", "".join(topcrates))
